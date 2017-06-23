dd44f7fb527b6171cb3b2aed67a873701170125f
Haoyou Liu <liuhaoyou1222@gmail.com>
2017-06-23 05:26:47 -0400

    Use config to pass hyperparameters

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -1,82 +1,95 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
-
 
 class cnn_clf(object):
     """
-    A CNN for text classification.
+    A CNN classifier for text classification
     """
-    def __init__(self,
-                 batch_size,
-                 sequence_length,
-                 num_classes,
-                 vocab_size,
-                 embedding_size,
-                 filter_sizes,
-                 num_filters,
-                 keep_prob,
-                 l2_reg_lambda=0.0,
-                 is_training=True):
+    def __init__(self, config, is_training=True):
+        self.sequence_length = config.sequence_length
+        self.num_classes = config.num_classes
+        self.batch_size = config.batch_size
+        self.vocab_size = config.vocab_size
+        self.embedding_size = config.embedding_size
+        self.filter_sizes = config.filter_sizes
+        self.num_filters = config.num_filters
+        self.keep_prob = config.keep_prob
+        self.learning_rate = config.learning_rate
+        self.l2_reg_lambda = config.l2_reg_lambda
 
-        self.input_x = tf.placeholder(dtype=tf.int32, shape=[batch_size, sequence_length])
-        self.input_y = tf.placeholder(dtype=tf.float32, shape=batch_size)
+        # Placeholder for input data and labels
+        self.input_x = tf.placeholder(dtype=tf.int32, shape=[self.batch_size, self.sequence_length])
+        self.input_y = tf.placeholder(dtype=tf.int64, shape=[self.batch_size])
 
-        l2_loss = tf.constant(0.0)
+        # L2 loss
+        self.l2_loss = tf.constant(0.0)
 
         # Word embedding
         with tf.device('/cpu:0'), tf.name_scope('embedding'):
-            embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                                         name='embedding')
-            inputs = tf.nn.embedding_lookup(embedding, self.input_x)
-            # Channel
-            inputs = tf.expand_dims(inputs, -1)
+            embedding = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0),
+                                    name="embedding")
+            embed = tf.nn.embedding_lookup(embedding, self.input_x)
+            inputs = tf.expand_dims(embed, -1)
 
-        # Convolution + maxpool
+        # Convolution & Maxpool
         pooled_outputs = []
-        for i, filter_size in enumerate(filter_sizes):
+        for i, filter_size in enumerate(self.filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
-                # Convolution Layer
-                filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                # Convolution
+                filter_shape = [filter_size, self.embedding_size, 1, self.num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1, name='weights'))
+                b = tf.Variable(tf.constant(0.1, shape=[self.num_filters]), name='biases')
                 conv = tf.nn.conv2d(inputs,
                                     W,
                                     strides=[1, 1, 1, 1],
-                                    padding="VALID",
-                                    name="conv")
-                # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                # Maxpooling over the outputs
+                                    padding='VALID',
+                                    name='conv')
+                # Activation function
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
+
+                # Maxpool
                 pooled = tf.nn.max_pool(h,
-                                        ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                                        ksize=[1, self.sequence_length - filter_size + 1, 1, 1],
                                         strides=[1, 1, 1, 1],
                                         padding='VALID',
-                                        name="pool")
+                                        name='pool')
                 pooled_outputs.append(pooled)
 
-        # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
-        self.h_pool = tf.concat(pooled_outputs, 3)
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        num_filters_total = self.num_filters * len(self.filter_sizes)
+        h_pool = tf.concat(pooled_outputs, 3)
+        h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
 
         # Add dropout
         with tf.name_scope("dropout"):
-            self.h_drop = tf.nn.dropout(self.h_pool_flat, keep_prob)
+            h_drop = tf.nn.dropout(h_pool_flat, keep_prob=self.keep_prob)
 
-        # Output
-        with tf.name_scope("output"):
-            W = tf.get_variable("W", shape=[num_filters_total, num_classes], initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
-            self.logits = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-            self.predictions = tf.argmax(self.logits, 1, name="predictions")
+        # Softmax
+        with tf.name_scope('Softmax'):
+            softmax_w = tf.Variable(tf.truncated_normal([num_filters_total, self.num_classes], stddev=0.1), name='softmax_w')
+            softmax_b = tf.Variable(tf.constant(0.1, shape=[self.num_classes]), name='softmax_b')
+
+            # Add L2 regularization to output layer
+            self.l2_loss += tf.nn.l2_loss(softmax_w)
+            self.l2_loss += tf.nn.l2_loss(softmax_b)
+
+            self.logits = tf.matmul(h_drop, softmax_w) + softmax_b
+            predictions = tf.nn.softmax(self.logits)
+            self.predictions = tf.argmax(predictions, 1)
 
         # Loss
-        with tf.name_scope("loss"):
-            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+        with tf.name_scope('Loss'):
+            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.input_y, logits=self.logits)
+            # Add L2 losses
+            self.cost = tf.reduce_mean(losses) + self.l2_reg_lambda * self.l2_loss
 
         # Accuracy
-        with tf.name_scope("accuracy"):
+        with tf.name_scope('Accuracy'):
             correct_predictions = tf.equal(self.predictions, self.input_y)
-            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+            self.correct_num = tf.reduce_sum(tf.cast(correct_predictions, tf.float32))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32), name='accuracy')
+
+        if not is_training:
+            return
+
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
+
--- rnn_classifier.py
+++ rnn_classifier.py
@@ -2,49 +2,54 @@
 import tensorflow as tf
 
 class rnn_clf(object):
-    def __init__(self,
-                 num_classes, # int
-                 batch_size,  # int
-                 vocab_size,  # int
-                 embedding_size,  # int
-                 hidden_size,  # int
-                 num_layers,  # int
-                 learning_rate,  # float
-                 keep_prob,  # float
-                 l2_reg_lambda=0.0,  # float
-                 is_training=True,  # boolean
-                 ):
+    """"
+    A RNN classifier for text classification
+    """
+    def __init__(self, config, is_training=True):
+        self.num_classes = config.num_classes
+        self.batch_size = config.batch_size
+        self.vocab_size = config.vocab_size
+        self.embedding_size = config.embedding_size
+        self.hidden_size = config.hidden_size
+        self.num_layers = config.num_layers
+        self.learning_rate = config.learning_rate
+        self.keep_prob = config.keep_prob
+        self.l2_reg_lambda = config.l2_reg_lambda
 
-        self.input_x = tf.placeholder(dtype=tf.int32, shape=[batch_size, None])
-        self.input_y = tf.placeholder(dtype=tf.int64, shape=batch_size)
-        self.sequence_length = tf.placeholder(dtype=tf.int32, shape=batch_size)
+        self.input_x = tf.placeholder(dtype=tf.int32, shape=[self.batch_size, None])
+        self.input_y = tf.placeholder(dtype=tf.int64, shape=[self.batch_size])
+        self.sequence_length = tf.placeholder(dtype=tf.int32, shape=[self.batch_size])
 
         # L2 loss
         self.l2_loss = tf.constant(0.0)
 
         # LSTM Cell
-        cell = tf.contrib.rnn.LSTMCell(hidden_size,
+        cell = tf.contrib.rnn.LSTMCell(self.hidden_size,
                                        forget_bias=1.0,
                                        state_is_tuple=True,
                                        reuse=tf.get_variable_scope().reuse)
         # Add dropout to cell output
-        if is_training and keep_prob < 1:
-            cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+        if is_training and self.keep_prob < 1:
+            cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.keep_prob)
 
         # Stacked LSTMs
-        cell = tf.contrib.rnn.MultiRNNCell([cell]*num_layers, state_is_tuple=True)
+        cell = tf.contrib.rnn.MultiRNNCell([cell]*self.num_layers, state_is_tuple=True)
 
-        self._initial_state = cell.zero_state(batch_size, dtype=tf.float32)
+        self._initial_state = cell.zero_state(self.batch_size, dtype=tf.float32)
 
         # Word embedding
         with tf.device('/cpu:0'), tf.name_scope('embedding'):
-            embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                                    name='embedding')
+            # embedding = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0),
+            #                         name='embedding')
+            # better performance
+            embedding = tf.get_variable('embedding',
+                                        shape=[self.vocab_size, self.embedding_size],
+                                        dtype=tf.float32)
             inputs = tf.nn.embedding_lookup(embedding, self.input_x)
 
         # Input dropout
-        if is_training and keep_prob < 1:
-            inputs = tf.nn.dropout(inputs, keep_prob=keep_prob)
+        if is_training and self.keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, keep_prob=self.keep_prob)
 
         # Dynamic LSTM
         with tf.variable_scope('LSTM'):
@@ -58,26 +63,29 @@ class rnn_clf(object):
 
         # Softmax output layer
         with tf.name_scope('Softmax'):
-            softmax_w = tf.get_variable('softmax_w', shape=[hidden_size, num_classes], dtype=tf.float32)
-            softmax_b = tf.get_variable('softmax_b', shape=[num_classes], dtype=tf.float32)
+            softmax_w = tf.get_variable('softmax_w', shape=[self.hidden_size, self.num_classes], dtype=tf.float32)
+            softmax_b = tf.get_variable('softmax_b', shape=[self.num_classes], dtype=tf.float32)
 
-            # L2 regularization
+            # L2 regularization for output layer
             self.l2_loss += tf.nn.l2_loss(softmax_w)
             self.l2_loss += tf.nn.l2_loss(softmax_b)
 
-            self.logits = tf.matmul(self.final_state[num_layers - 1].h, softmax_w) + softmax_b
+            self.logits = tf.matmul(self.final_state[self.num_layers - 1].h, softmax_w) + softmax_b
             predictions = tf.nn.softmax(self.logits)
             self.predictions = tf.argmax(predictions, 1)
 
         # Loss
         with tf.name_scope('Loss'):
             tvars = tf.trainable_variables()
+
+            # L2 regularization for LSTM weights
             for tv in tvars:
                 if 'kernel' in tv.name:
                     self.l2_loss += tf.nn.l2_loss(tv)
+
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.input_y,
                                                                   logits=self.logits)
-            self.cost = tf.reduce_mean(loss) + l2_reg_lambda * self.l2_loss
+            self.cost = tf.reduce_mean(loss) + self.l2_reg_lambda * self.l2_loss
 
         # Accuracy
         with tf.name_scope('Accuracy'):
@@ -89,4 +97,4 @@ class rnn_clf(object):
             return
 
         # Optimizer
-        self.train_op = tf.train.AdamOptimizer(learning_rate).minimize(self.cost)
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
