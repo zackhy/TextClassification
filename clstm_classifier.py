972d4fc79d64bca59c1f72573a02a3d21eb02abc
Haoyou Liu <liuhaoyou1222@gmail.com>
2017-06-28 04:18:12 -0400

    C-LSTM initial commit

---
+++ clstm_classifier.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+
+class clstm_clf(object):
+    """
+    A C-LSTM classifier for text classification
+    """
+    # TODO one convolution layer and one LSTM layer
+    # TODO add dropout to the input before CNN and the output before softmax layer
+    # TODO add L2 regularization to softmax layer
+    # TODO hyperparameters include number of filters, filter sizes, hidden size, dropout rate, L2
+
